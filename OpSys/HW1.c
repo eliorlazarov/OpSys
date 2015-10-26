0b74cf795471bfc12593faacbbff9c186f4dc452
ofrinevo <ofrine@gmail.com>
2015-10-26 19:00:30 +0200

    Uqawecf

--- OpSys/HW1.c
+++ OpSys/HW1.c
@@ -11,37 +11,43 @@
 
 int main(int argc, char* argv[])
 {
-
 	char temp;
 	char currKey;
 	char curr;
 	char* buf[1];
 	if (argc != 4){
-		printf("Not enough arguments/n");
+		printf("Not enough arguments\n");
 		return 1;
 	}
-	char buf[1];
-	int fd = open(argv[1]);
-	int fdWrite = open(argv[3]);
+	int fd = open(argv[1],O_RDONLY);
+	
 
 	if (fd < 0){
 		printf("Error opening file: %s\n", strerror(errno));
 		return errno;
 	}
+	int fdWrite = open(argv[3],O_WRONLY);
 	if (fdWrite < 0){
 		printf("Error opening file: %s\n", strerror(errno));
+		close(fd);
 		return errno;
 	}
-	int fdKey = open(argv[2]);
+	int fdKey = open(argv[2],O_RDONLY);
 	if (fdKey < 0){
 		printf("Error opening file: %s\n", strerror(errno));
+		close(fd);
+		close(fdWrite);
 		return errno;
 	}
 	while (curr = read(fd, buf, 1) == 1 && curr != EOF){
 		currKey = lseek(fdKey, 1, SEEK_CUR);
 		if (currKey == EOF)
 			currKey = lseek(fdKey, 0, SEEK_SET);
-		buf[0] = (currKey^curr);
+		buf[0] = (void*)(currKey^curr);
 		write(fdWrite, buf, 1);
 	}
+    close(fd);
+    close(fdWrite);
+    close(fdKey);
+    return 0;
 }
