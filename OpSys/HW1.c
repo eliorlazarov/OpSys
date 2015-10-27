0e5dae6668c883a21e1dae181e7293760d9c7fc3
ofrinevo <ofrine@gmail.com>
2015-10-27 19:28:16 +0200

    uasd

--- OpSys/HW1.c
+++ OpSys/HW1.c
@@ -11,37 +11,83 @@
 
 int main(int argc, char* argv[])
 {
+	struct dirent *dp;
+	int fd,fdWrite;
 	int place;
 	char temp;
 	char currKey;
 	char curr;
 	char buf[1];
 	char bufKey[1];
+	DIR* dfd;
+	DIR* dirWrite;
+	//char* dir,write;
 	int k;
+	char fileName[100];
+	char fileWrite[100];
 	if (argc != 4){
 		printf("Not enough arguments\n");
 		return 1;
 	}
-	int fd = open(argv[1],O_RDONLY);
 	
+	if((dfd=opendir(argv[1]))==NULL){
+	  
+	//int fd = open(argv[1],O_RDONLY);
+	printf("Error opening dir: %s\n", strerror(errno));
+		return errno;
+	}
 
-	if (fd < 0){
-		printf("Error opening file: %s\n", strerror(errno));
+
+	if((dirWrite=opendir(argv[3]))==NULL){
+	  printf("Error opening dir: %s\n", strerror(errno));
 		return errno;
 	}
-	int fdWrite = open(argv[3],O_WRONLY);
-	if (fdWrite < 0){
-		printf("Error opening file: %s\n", strerror(errno));
-		close(fd);
-		return errno;
-	}
+
 	int fdKey = open(argv[2],O_RDONLY);
+
 	if (fdKey < 0){
 		printf("Error opening file: %s\n", strerror(errno));
 		close(fd);
 		close(fdWrite);
 		return errno;
 	}
+		
+	while((dp=readdir(dfd))!=NULL){
+	  struct stat statbuf;
+	sprintf(fileName,"%s/%s",argv[1],dp->d_name);
+	printf("fileName is: %s\n",fileName);
+
+	if((statbuf.st_mode & S_IFMT)==S_IFDIR || !(strcmp(dp->d_name,".")) || !(strcmp(dp->d_name,"..")))
+	  continue;
+	
+	  else{
+	if(stat(fileName,&statbuf)==-1){
+	  printf("Error reading file: %s\n", strerror(errno));
+		close(fd);
+		close(fdWrite);
+		return errno;
+	}
+	  char str[100];
+	  strcpy(str,argv[3]);
+	   strcat(str,"/");
+	  strcat(str,dp->d_name);
+	  
+	  printf("str is: %s\n",str);
+	  	fdWrite = open(str,O_WRONLY | O_CREAT | O_TRUNC);
+	if (fdWrite < 0){
+		printf("Error opening file: %s\n", strerror(errno));
+		close(fd);
+		return errno;
+	}
+	  fd = open(argv[1],O_RDONLY);
+	  if(fd<0){
+		printf("Error opening file: %s\n", strerror(errno));
+		close(fd);
+		close(fdWrite);
+		return errno;
+	}
+	}
+	
 	while (read(fd, buf, 1) == 1 && curr != EOF){
 	  k=read(fdKey,bufKey,1);
 		if(k<0){
@@ -55,6 +101,14 @@ int main(int argc, char* argv[])
 		else if (k==0){
 		  lseek(fdKey, 0, SEEK_SET);
 		  k=read(fdKey,bufKey,1);
+		  if(k<0){
+		  printf("Error reading file: %s\n",strerror(errno));
+
+		  close(fd);
+		  close(fdWrite);
+		  close(fdKey);
+		  return errno;
+		}
 		}
 		
 		buf[0] = (bufKey[0] ^ buf[0]);
@@ -66,6 +120,7 @@ int main(int argc, char* argv[])
 		  return errno;
 		}
 
+	}
 	}
     close(fd);
     close(fdWrite);
