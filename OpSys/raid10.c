2ad8a0a4a63e51d6e797cdbe609c998ee63dcc6f
ofrinevo <ofrine@gmail.com>
2015-11-13 16:27:57 +0200

    qwedasfeyhtj

--- OpSys/raid10.c
+++ OpSys/raid10.c
@@ -28,7 +28,7 @@ int findWorkingDevice(int dev0) {
 		if (dev_fd[dev0][i] >= 0)
 			return i;
 	}
-
+	
 	return -1;
 }
 
@@ -48,13 +48,13 @@ void closeAll() {
 void do_raid0_rw(char* operation, int sector, int count)
 {
 	int i = sector;
-
+	
 	while (i < sector + count)
 	{
 		// find the relevant device for current sector
 		int block_num = i / SECTORS_PER_BLOCK;
 		int dev_num = block_num % num_dev0;
-
+		
 		// make sure device didn't fail
 		int defaultDev = findWorkingDevice(dev_num);
 		if (defaultDev == -1)
@@ -64,25 +64,25 @@ void do_raid0_rw(char* operation, int sector, int count)
 		}
 		// find offset of sector inside device
 		int block_start = i / (1 * SECTORS_PER_BLOCK);
-
+		
 		int block_off = i % SECTORS_PER_BLOCK;
 		int sector_start = block_start * SECTORS_PER_BLOCK + block_off;
 		int offset = sector_start * SECTOR_SIZE;
-
+		
 		// try to write few sectors at once
 		int num_sectors = SECTORS_PER_BLOCK - block_off;
 		while (i + num_sectors > sector + count)
 			--num_sectors;
 		int sector_end = sector_start + num_sectors - 1;
 		int size = num_sectors * SECTOR_SIZE;
-
+		
 		// validate calculations
 		assert(num_sectors > 0);
 		assert(size <= BUFFER_SIZE);
 		assert(offset + size <= DEVICE_SIZE);
 		// seek in relevant device
 		assert(offset == lseek(dev_fd[dev_num][defaultDev], offset, SEEK_SET));
-
+		
 		if (!strcmp(operation, "READ")) {
 			while (1) {
 				if (size != read(dev_fd[dev_num][defaultDev], buf, size)) {
@@ -98,13 +98,13 @@ void do_raid0_rw(char* operation, int sector, int count)
 				}
 				else {
 					printf("Operation on device %d, sector %d-%d\n",
-						dev_num*num_dev1 + defaultDev, sector_start, sector_end);
-
+					       dev_num*num_dev1 + defaultDev, sector_start, sector_end);
+					
 					break;
 				}
 			}
-
-
+			
+			
 		}
 		else if (!strcmp(operation, "WRITE"))
 			for (int j = 0; j < num_dev1; j++) {
@@ -116,9 +116,9 @@ void do_raid0_rw(char* operation, int sector, int count)
 					}
 					else
 						printf("Operation on device %d, sector %d-%d\n",
-							dev_num*num_dev1 + defaultDev, sector_start, sector_end);
+						       dev_num*num_dev1 + defaultDev, sector_start, sector_end);
 			}
-		i += num_sectors;
+			i += num_sectors;
 	}
 }
 
@@ -126,20 +126,20 @@ void do_raid0_rw(char* operation, int sector, int count)
 int main(int argc, char** argv)
 {
 	assert(argc > 2);
-
+	
 	int i;
 	char line[1024];
 	char rep[1024];
 	// number of devices == number of arguments (ignore 1st)
-
+	
 	num_dev0 = atoi(argv[1]);
-
+	
 	num_dev1 = (argc - 2) / num_dev0;
-
+	
 	int _dev_fd[num_dev0][num_dev1];
-
-
-
+	
+	
+	
 	dev_fd = (int**)malloc(num_dev0*sizeof(int*));
 	for (int i = 0; i < num_dev0; i++)
 		dev_fd[i] = (int*)malloc(num_dev1*sizeof(int));
@@ -147,13 +147,14 @@ int main(int argc, char** argv)
 	for (i = 0; i < num_dev0; i++) {
 		for (int j = 0; j < num_dev1; j++) {
 			printf("Opening device %d: %s\n", i*num_dev1 + j, argv[i*num_dev1 + j + 2]);
-
+			
 			dev_fd[i][j] = open(argv[i*num_dev1 + j + 2], O_RDWR);
-			printf("%d\n", dev_fd[i][j]);
-			assert(dev_fd[i][j] >= 0);
+			if(dev_fd[i][j] < 0){
+				dev_fd[i][j]=-1;
+			}
 		}
 	}
-
+	
 	// vars for parsing input line
 	char operation[20];
 	int sector;
@@ -166,7 +167,7 @@ int main(int argc, char** argv)
 	// read input lines to get command of type "OP <SECTOR> <COUNT>"
 	while (fgets(line, 1024, stdin) != NULL) {
 		assert(sscanf(line, "%s %d %s", operation, &sector, rep) == 3);
-
+		
 		// KILL specified device
 		if (!strcmp(operation, "KILL")) {
 			assert(!close(dev_fd[sector / num_dev0][sector % num_dev1]));
@@ -174,7 +175,7 @@ int main(int argc, char** argv)
 		}
 		else if (!strcmp(operation, "REPAIR")) {
 			close(dev_fd[sector / num_dev0][sector % num_dev1]);
-
+			
 			dev_fd[sector / num_dev0][sector % num_dev1] = -1;
 			if ((reDevice = open(rep, O_RDWR)) < 0)
 				continue;
@@ -189,14 +190,14 @@ int main(int argc, char** argv)
 					if (lseek(dev_fd[sector / num_dev0][currWork], c, SEEK_SET) == -1)
 						continue;
 					if ((sizeRead = read(dev_fd[sector / num_dev0][currWork], buf, 1024 * 1024)) != -1) {
-
+						
 						c += sizeRead;
-
-
+						
+						
 					}
 					if (c == DEVICE_SIZE) {//Done copying
 						dev_fd[sector / num_dev0][sector % num_dev1] = reDevice;
-
+						
 						break;
 					}
 				}
