bf5c9f71792030cf61ac7d73473f350ce7175b8c
ofrinevo <ofrine@gmail.com>
2015-11-24 00:14:04 +0200

    tyu

--- OpSys/copy_tool.c
+++ OpSys/copy_tool.c
@@ -17,31 +17,31 @@ int main(int argc, char** argv) {
 	int fd1 = open(argv[1], O_RDWR), fd2;
 	int size, t;
 	char *arr1, *arr2;
-
-
+	
+	
 	if (fd1 < 0) {
 		if (errno == ENOENT) {
 			printf("Source file doesn't exist!\n");
 		}
 		else {
-			printError("open");
+			printf("Error opening file, quiting.\n");
 		}
 		return -1;
 	}
-
-
+	
+	
 	fd2 = open(argv[2], O_CREAT | O_EXCL | O_RDWR, S_IRWXO | S_IRWXG | S_IRWXU);
-
+	
 	if (fd2 < 0 && errno == EEXIST) {
 		printf("The destination file already exists!\n");
 		return -1;
 	}
 	else if (fd2 < 0) {
-		prinf("Error opening file, quiting.\n");
+		printf("Error opening file, quiting.\n");
 		return -1;
 	}
 	;
-
+	
 	if ((size = lseek(fd1, 1, SEEK_END)) == -1) {
 		printf("Error while using lseek, quiting.\n");
 		close(fd1);
@@ -49,16 +49,16 @@ int main(int argc, char** argv) {
 		unlink(argv[2]);
 		return -1;
 	}
-
+	
 	if ((t=truncate(argv[2], size)) == -1) {
 		close(fd1);
 		close(fd2);
 		unlink(argv[2]);
 		printf("Error using truncate, quiting.\n");
 	}
-
+	
 	arr1 = (char*)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd1, 0);
-
+	
 	if (arr1 == MAP_FAILED) {
 		close(fd1);
 		close(fd2);
@@ -66,9 +66,9 @@ int main(int argc, char** argv) {
 		printf("Error mmapping the source file, quiting.\n");
 		return -1;
 	}
-
+	
 	arr2 = (char*)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd2, 0);
-
+	
 	if (arr2 == MAP_FAILED) {
 		close(fd1);
 		close(fd2);
@@ -76,11 +76,11 @@ int main(int argc, char** argv) {
 		printf("Error mmapping the destination file, quiting.\n");
 		return -1;
 	}
-
+	
 	for (t = 0; t < size; t++) {
 		arr2[t] = arr1[t];
 	}
-
+	
 	if (munmap(arr1, size) == -1) {
 		printf("Error un-mmapping the source file\n");
 		close(fd1);
@@ -88,8 +88,8 @@ int main(int argc, char** argv) {
 		unlink(argv[2]);
 		return -1;
 	}
-
-
+	
+	
 	if (munmap(arr2, size) == -1) {
 		printf("Error un-mmapping the destination file\n");
 		close(fd1);
@@ -97,16 +97,16 @@ int main(int argc, char** argv) {
 		unlink(argv[2]);
 		return -1;
 	}
-
+	
 	if (close(fd1) < 0) {
 		printf("Error closing, quiting.\n");
 		return -1;
 	}
-
+	
 	if (close(fd2) < 0) {
 		printf("Error closing, quiting.\n");
 		return -1;
 	}
 	return 0;
-
+	
 }
