59c1aacc586612669bbcea80963688d0ad5348ed
ofrinevo <ofrine@gmail.com>
2015-12-02 15:56:53 +0200

    issue 45

--- OpSys/writer_pipe.c
+++ OpSys/writer_pipe.c
@@ -14,7 +14,7 @@ int fd;
 char *path;
 
 void IntTermHandler(int signum){
-
+	
 	assert(unlink(path) >= 0); // removes the FIFO file
 	assert(close(fd) >= 0); // closes the FIFO file
 	exit(-1);
@@ -29,16 +29,16 @@ int main(int argc, char** argv){
 	assert(argc == 2);
 	struct stat stat_buf;
 	path = argv[1];
-
+	
 	struct sigaction action_IntTerm;
 	action_IntTerm.sa_handler = IntTermHandler;
 	sigaction(SIGINT, &action_IntTerm, NULL);
 	sigaction(SIGTERM, &action_IntTerm, NULL);
-
+	
 	struct sigaction action_Pipe;
 	action_Pipe.sa_handler = PipeHandler;
 	sigaction(SIGPIPE, &action_Pipe, NULL);
-
+	
 	fd = open(path, O_WRONLY, S_IRWXO|S_IRWXG|S_IRWXU);
 	if(fd < 0){ 
 		if(errno == ENOENT){ // file does not exist
@@ -65,24 +65,24 @@ int main(int argc, char** argv){
 		if ((stat_buf.st_mode & S_IFMT) != S_IFIFO) { // not a FIFO file
 			
 			assert(unlink(path) >= 0);
-
+			
 			if(mkfifo(path, S_IRWXO|S_IRWXG|S_IRWXU) < 0){ // error occurred
 				printf("Error occurred.\n");
 				printf("%s\n", strerror(errno));
 				return -1;
 			}
 			fd = open(path, O_WRONLY, S_IRWXO|S_IRWXG|S_IRWXU);	
-        }
+		}
 	}
-
+	
 	char line[1024];
-
+	
 	while (fgets(line, 1024, stdin) != NULL) {
 		write(fd, line, 1024);
 	}
-
+	
 	assert(unlink(path) >= 0); // removes the FIFO file
 	assert(close(fd) >= 0); // closes the FIFO file
-
+	
 	return 0;
 }
