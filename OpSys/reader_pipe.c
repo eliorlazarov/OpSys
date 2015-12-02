18a3c04a93496598fd806f58b4c5f306e129f979
ofrinevo <ofrine@gmail.com>
2015-12-02 15:56:32 +0200

    issue 4

--- OpSys/reader_pipe.c
+++ OpSys/reader_pipe.c
@@ -16,7 +16,8 @@ int main(int argc, char** argv) {
 	int fd;
 	char line[1024];
 	struct sigaction prev;
-	prev.sa_handler = SIG_IGN;
+	struct sigaction after;
+	after.sa_handler = SIG_IGN;
 	
 	while (1) {
 		fd = open(argv[1], O_RDONLY);
@@ -40,8 +41,9 @@ int main(int argc, char** argv) {
 			printf("It is not a FIFO file!\n");
 			return -1;
 		}
-		sigaction(SIGINT, NULL, &prev);
-		sigaction(SIGTERM, NULL, &prev);
+		sigaction(SIGINT, &after, &prev);
+		sigaction(SIGTERM, &after, &prev);
+		
 		while (read(fd, line, 1024) > 0) 
 			printf("%s", line);
 		if (close(fd) < 0) {
