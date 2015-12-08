e364c4e0eaf5a8ee931dab7e479486dfc9423f19
ofrinevo <ofrine@gmail.com>
2015-12-08 22:29:30 +0200

    fyfyi

---
+++ OpSys/myshell.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+
+int contains(int count, char** arglist, char let) {
+	int i;
+	for (i = 0; i < count; i++) {
+		if (arglist[i][0] == let)
+			return 1;
+	}
+	return 0;
+}
+
+int process_arglist(int count, char** arglist) {
+	int conAmp = contains(count, arglist, '&');
+	int conPipe = contains(count, arglist, '|');
+	execute(arglist, conAmp, conPipe);
+}
+
+void  execute(char **argv, int conAmp,int conPipe)
+{
+	pid_t  pid;
+	int    status;
+
+	if ((pid = fork()) < 0) {     /* fork a child process           */
+		printf("*** ERROR: forking child process failed\n");
+		exit(1);
+	}
+	else if (pid == 0) {          /* for the child process:         */
+		if (execvp(*argv, argv) < 0) {     /* execute the command  */
+			printf("*** ERROR: exec failed\n");
+			exit(1);
+		}
+	}
+	else {                                  /* for the parent:      */
+		while (wait(&status) != pid)       /* wait for completion  */
+			;
+	}
+}
