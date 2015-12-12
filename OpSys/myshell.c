42f9e85ff47099209bd1e3d32fa8364c2d79001d
ofrinevo <ofrine@gmail.com>
2015-12-12 21:37:41 +0200

    y78

--- OpSys/myshell.c
+++ OpSys/myshell.c
@@ -9,7 +9,7 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
-
+#include "input.c"
 int contains(int count, char** arglist, char let) {
 	int i;
 	for (i = 0; i < count; i++) {
@@ -22,7 +22,11 @@ int contains(int count, char** arglist, char let) {
 int process_arglist(int count, char** arglist) {
 	int conAmp = contains(count, arglist, '&');
 	int conPipe = contains(count, arglist, '|');
-	execute(arglist, conAmp, conPipe);
+	if(conPipe+conAmp==0)
+		execute(arglist, conAmp, conPipe);
+	else if (conPipe) {
+
+	}
 }
 
 void  execute(char **argv, int conAmp,int conPipe)
@@ -45,23 +49,52 @@ void  execute(char **argv, int conAmp,int conPipe)
 				;
 		}
 	}
-	else if (conPipe) {
-		int pipefd[2];
-		assert(pipe(pipefd) != -1);
-		int procID = fork();
-		if (procID < 0) { // fork failed
-			perror("fork failed");
-		}
-		if (procID == 0) { // child process
-			dup2(pipefd[0], fileno(stdin));
-			close(pipefd[1]);
-		}
-		else { // parent process
-			dup2(pipefd[1], fileno(stdout));
-			close(pipefd[0]);
-			if (execvp(*argv, argv) == -1) {
-				perror("execvp failed");
-			}
+	
+}
+void np_exec(char* cmd, char** argv)
+{
+	int pipefd[2];
+	assert(pipe(pipefd) != -1);
+	int procID = fork();
+	if (procID < 0) { // fork failed
+		perror("fork failed");
+	}
+	if (procID == 0) { // child process
+		dup2(pipefd[0], fileno(stdin));
+		close(pipefd[1]);
+	}
+	else { // parent process
+		dup2(pipefd[1], fileno(stdout));
+		close(pipefd[0]);
+		if (execvp(cmd, argv) == -1) {
+			perror("execvp failed");
 		}
 	}
+}
+
+int execPipe(int argc, char** argv)
+{
+	assert(strcmp(argv[argc - 1], "-")); // check that the last argument is not "-"
+
+	int i;
+	for (i = 1; i < argc; ++i) {
+		if (!strcmp(argv[i], "-")) { // if argv[i] is "-"
+			argv[i] = NULL;
+			np_exec(argv[1], &argv[1]);
+			argv = &argv[i];
+			argc -= i;
+			i = 0;
+		}
+	}
+
+	char* args[argc];
+	args[argc - 1] = NULL;
+
+	for (i = 1; i < argc; ++i) {
+		args[i - 1] = argv[i];
+	}
+
+	if (execvp(args[0], args) == -1)
+		perror("execvp failed");
+	return;
 }
