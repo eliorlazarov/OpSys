2dd273e6f678bebbc05318c1732e1947a1ac09d3
ofrinevo <ofrine@gmail.com>
2015-12-22 21:48:57 +0200

    wegretjtyk

--- OpSys/myshell.c
+++ OpSys/myshell.c
@@ -1,3 +1,15 @@
+/*checks if the list had & or |.
+for each case, run the right command.
+if it has &, run the command on a child process, 
+and wait for it in the father proc to prevent zombies.
+if it has |, run each command on a different child
+connect the stdout and stdin so the first commands prints
+to the second command and not to the screen.
+wait for both proc to prevent zombies, and join the threads in the end.
+if it doesn't have either, just run the command as-is
+*/
+
+
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
@@ -13,13 +25,14 @@
 int execute(char **argv);
 int pipeExec(char** argv,int i);
 
+//the waiting function
 void* waiter(void* arg){
 	int status;
 	int arg1=(int) arg;
 	waitpid((int)arg1,&status,1);
 }
 
-
+//checks if it has a specific symobl and return it's location
 int contains(int count, char** arglist, char let) {
 	int i;
 	for (i = 0; i < count; i++) {
@@ -42,7 +55,7 @@ int process_arglist(int count, char** arglist) {
 	
 	return 1;
 }
-
+//For the & case
 int ampExec(char **argv,int count){
 	int pt;
 	pid_t pid;
@@ -54,7 +67,7 @@ int ampExec(char **argv,int count){
 		exit(1);
 	}
 	else if (pid == 0) {//If child
-		if (execvp(*argv, argv) < 0) {
+		if (execvp(argv[0], argv) < 0) {
 			perror("exec failed\n");
 			return 0;
 		}
@@ -68,7 +81,7 @@ int ampExec(char **argv,int count){
 	}
 	return 1;
 }
-
+//For the regular case
 int execute(char **argv){
 	
 	pid_t pid;
@@ -79,20 +92,22 @@ int execute(char **argv){
 		perror("Fork failed");
 		exit(1);
 	}
-	else if (pid == 0) {//If child
-		if (execvp(*argv, argv) < 0) {
+	if (pid == 0) {//If child
+		if (execvp(argv[0], argv) < 0) {
 			perror("execvp failed\n");
 			return 0;
 		}
 	}
-	else {
-		while (wait(&status) != pid)
-			;
+	else {//Parent
+		while (1)
+			if(wait(&status) != pid);
+				break;
 	}
 	
 	return 1;
 }
-//Based on stackoverflow code
+//with some help from stackoverflow
+//for the | case
 int pipeExec(char** argv,int i){
 	
 	pid_t pid1,pid2;
@@ -116,12 +131,12 @@ int pipeExec(char** argv,int i){
 		close(pipeArr[1]);
 		execvp(argv[0], argv);
 		perror("execvp failed");//Should only get here in error
-		exit(1);
+		exit(0);
 		
 	}
 	pthread_t t1;
-	int returnC=pthread_create(&t1,NULL,waiter,(void*)pid1);
-	if(returnC){
+	int r=pthread_create(&t1,NULL,waiter,(void*)pid1);
+	if(r){
 		perror("pthread create");
 	}
 	if((pid2=fork()) < 0){ //Handle the second cmd
@@ -137,23 +152,23 @@ int pipeExec(char** argv,int i){
 		close(pipeArr[0]);
 		execvp(argv2[0], argv2);
 		perror("execvp failed");//Should only get here in error
-		exit(1);
+		exit(0);
 		
 	}
 	close(pipeArr[0]);
 	close(pipeArr[1]);
 	pthread_t t2;
-	returnC=pthread_create(&t2,NULL,waiter,(void*)pid2);
-	if(returnC){
+	r=pthread_create(&t2,NULL,waiter,(void*)pid2);
+	if(r){
 		perror("pthread create");
 	}
-	returnC=pthread_join(t1,NULL);
-	if(returnC){
+	r=pthread_join(t1,NULL);
+	if(r){
 		perror("join1");
 		exit(1);
 	}
-	returnC=pthread_join(t2,NULL);
-	if(returnC){
+	r=pthread_join(t2,NULL);
+	if(r){
 		perror("join2");
 		exit(1);
 	}
